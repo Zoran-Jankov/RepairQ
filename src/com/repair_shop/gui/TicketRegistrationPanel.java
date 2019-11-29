43c4461a1203d5a3ea3e4d47269e6e54225963cb
Zoran Jankov <54670722+EmperorZoran@users.noreply.github.com>
2019-11-29 16:04:57 +0100

    Update TicketRegistrationPanel.java

--- src/com/repair_shop/gui/TicketRegistrationPanel.java
+++ src/com/repair_shop/gui/TicketRegistrationPanel.java
@@ -2,7 +2,6 @@ package com.repair_shop.gui;
 
 import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
-import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JEditorPane;
@@ -10,16 +9,31 @@ import javax.swing.JEditorPane;
 public class TicketRegistrationPanel extends JPanel
 {
 	private static final long serialVersionUID = -7560124269727203908L;
+	private JEditorPane txtDetails = new JEditorPane();
+	private JEditorPane txtImportantNotes = new JEditorPane();
 	
 	public TicketRegistrationPanel()
 	{
-		setLayout(new MigLayout("", "[grow]", "[][::120px,grow]"));
+		setLayout(new MigLayout("", "[grow]", "[20px:n][80px:n,grow][20px:n][50px:n:80px,grow]"));
 		
-		JLabel lblDetails = LabelFactory.createJLabel("Malfunction", new Font("Tahoma", Font.PLAIN, 13));
+		JLabel lblDetails = LabelFactory.createJLabel("Details", new Font("Tahoma", Font.PLAIN, 13));
 		add(lblDetails, "cell 0 0,alignx center,growy");
 		
-		JEditorPane editorPane = new JEditorPane();
-		add(editorPane, "cell 0 1,grow");
+		add(txtDetails, "cell 0 1,grow");
 		
+		JLabel lblImportantNotes = LabelFactory.createJLabel("Important Notes", new Font("Tahoma", Font.PLAIN, 13));
+		add(lblImportantNotes, "cell 0 2,alignx center,growy");
+		
+		add(txtImportantNotes, "cell 0 3,grow");
+	}
+	
+	public String getDetails()
+	{
+		return txtDetails.getText();
+	}
+	
+	public String getImportantNotes()
+	{
+		return txtImportantNotes.getText();
 	}
 }
