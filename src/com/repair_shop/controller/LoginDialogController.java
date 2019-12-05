7bca6458d3768d28d8b6d6ea679e518f2dd2f4a8
Zoran Jankov <zoran.jankov.87@gmail.com>
2019-12-06 00:56:41 +0100

    Create LoginDialogController.java

---
+++ src/com/repair_shop/controller/LoginDialogController.java
@@ -0,0 +1,42 @@
+package com.repair_shop.controller;
+
+import com.repair_shop.gui.LoginDialog;
+
+public class LoginDialogController
+{
+	private LoginDialog gui = new LoginDialog();
+	
+	public LoginDialogController()
+	{
+		gui.getInputButtonPanel().setBtnAddActionListener(null);
+		gui.getInputButtonPanel().setBtnCancelActionListener(null);
+	}
+	
+	public void tryLogin()
+	{
+		if(isInputValid())
+		{
+			Login();
+		}
+		else
+		{
+			showLoginErrors();
+		}
+	}
+
+	private boolean isInputValid()
+	{
+		// TODO Implement isInputValid() method
+		return false;
+	}
+
+	private void Login()
+	{
+		// TODO Implement Login() method
+	}
+
+	private void showLoginErrors()
+	{
+		// TODO Implement showLoginErrors() method
+	}
+}
