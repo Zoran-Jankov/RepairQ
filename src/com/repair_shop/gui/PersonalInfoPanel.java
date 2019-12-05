2ffb0651ab77dfa35312266fdc86e3ed7510189b
Zoran Jankov <zoran.jankov.87@gmail.com>
2019-12-05 19:31:34 +0100

    Update PersonalInfoPanel.java

--- src/com/repair_shop/gui/PersonalInfoPanel.java
+++ src/com/repair_shop/gui/PersonalInfoPanel.java
@@ -11,11 +11,11 @@ import java.awt.Font;
 public class PersonalInfoPanel extends JPanel
 {
 	private static final long serialVersionUID = -1636004925810635460L;
-	private JTextField txtName;
-	private JTextField txtPrimePhone;
-	private JTextField txtAltPhone;
-	private JTextField txtEmail;
-	private JTextField txtAddress;
+	private JTextField txtName = TextFieldFactory.createJTextField(10);
+	private JTextField txtPrimePhone = TextFieldFactory.createJTextField(10);;
+	private JTextField txtAltPhone = TextFieldFactory.createJTextField(10);;
+	private JTextField txtEmail = TextFieldFactory.createJTextField(10);
+	private JTextField txtAddress = TextFieldFactory.createJTextField(10);
 
 	public PersonalInfoPanel()
 	{
@@ -24,31 +24,26 @@ public class PersonalInfoPanel extends JPanel
 		JLabel lblName = LabelFactory.createJLabel("First and Last Name", new Font("Tahoma", Font.PLAIN, 13));
 		add(lblName, "cell 0 0,growy");
 		
-		txtName = TextFieldFactory.createJTextField(10);
 		add(txtName, "cell 2 0,grow");
 		
 		JLabel lblPrimePhone = LabelFactory.createJLabel("Primary Phone Number", new Font("Tahoma", Font.PLAIN, 13));
 		add(lblPrimePhone, "cell 0 1,growy");
 		
-		txtPrimePhone = TextFieldFactory.createJTextField(10);
 		add(txtPrimePhone, "cell 2 1,grow");
 		
 		JLabel lblAltPhone = LabelFactory.createJLabel("Alternative Phone Number", new Font("Tahoma", Font.PLAIN, 13));
 		add(lblAltPhone, "cell 0 2,growy");
 		
-		txtAltPhone = TextFieldFactory.createJTextField(10);
 		add(txtAltPhone, "cell 2 2,grow");
 		
 		JLabel lblEmail = LabelFactory.createJLabel("Email Address", new Font("Tahoma", Font.PLAIN, 13));
 		add(lblEmail, "cell 0 3,growy");
 		
-		txtEmail = TextFieldFactory.createJTextField(10);
 		add(txtEmail, "cell 2 3,growx");
 		
 		JLabel lblHomeAddress = LabelFactory.createJLabel("Home Address", new Font("Tahoma", Font.PLAIN, 13));
 		add(lblHomeAddress, "cell 0 4,growy");
 		
-		txtAddress = TextFieldFactory.createJTextField(10);
 		add(txtAddress, "cell 2 4,grow");
 	}
 	
