88ed56a7646beb265d5b11303d99a7a7f770c89a
Zoran Jankov <zoran.jankov.87@gmail.com>
2019-12-07 00:22:07 +0100

    Update LabelName.java

--- src/com/repair_shop/utility/LabelName.java
+++ src/com/repair_shop/utility/LabelName.java
@@ -1,5 +1,25 @@
 package com.repair_shop.utility;
 
-public class LabelName {
-
+public class LabelName
+{
+	public static String NAME;
+	public static String DESCRIPTION;
+	
+	public static String CLIENT;
+	
+	public static String FIRST_AND_LAST_NAME;
+	public static String PRIME_PHONE;
+	public static String ALTERNATIVE_PHONE;
+	public static String EMAIL;
+	public static String ADDRESS;
+	
+	public static String MARKETING;
+	
+	public static String USER;
+	public static String USER_TYPE;
+	
+	public static String DEVICE_TYPE;
+	public static String BRAND;
+	public static String MODEL;
+	public static String SERIAL;
 }
