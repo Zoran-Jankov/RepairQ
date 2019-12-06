eec3a7bfd2c1ac59d45e589e9b128ed882953f1c
Zoran Jankov <54670722+EmperorZoran@users.noreply.github.com>
2019-12-06 15:22:55 +0100

    Create Title.java

---
+++ src/com/repair_shop/utility/Title.java
@@ -0,0 +1,21 @@
+package com.repair_shop.utility;
+
+public class Title
+{
+	public static final String NOTIFICATION = "New Notification";
+	public static final String NOTIFICATION_TYPE = "Notification Type Registration";
+	public static final String SERVICE = "New Service";
+	public static final String SERVICE_TYPE = "Service Type Registration";
+	public static final String TICKET = "New Ticket";
+	public static final String STATUS = "Status Registration";
+	public static final String CLIENT = "Client Registration";
+	public static final String MARKETING_TYPE = "Marketing Registration";
+	public static final String LEGAL_ENTITY = "Lefal Entity Registration";
+	public static final String DEVICE = "Device Registration";
+	public static final String MODEL = "Model Registration";
+	public static final String DEVICE_TYPE = "Device Type Registration";
+	public static final String BRAND = "Brand Registration";
+	public static final String USER = "User Registration";
+	
+	public static String PROPERTY;
+}
