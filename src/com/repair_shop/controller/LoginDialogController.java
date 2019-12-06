43e4ee95ce3be86094ae4c0d8386054226f1b352
Zoran Jankov <zoran.jankov.87@gmail.com>
2019-12-07 00:27:21 +0100

    Update LoginDialogController.java

--- src/com/repair_shop/controller/LoginDialogController.java
+++ src/com/repair_shop/controller/LoginDialogController.java
@@ -16,7 +16,7 @@ public class LoginDialogController
 	{
 		if(isInputValid())
 		{
-			Login();
+			login();
 		}
 		else
 		{
@@ -30,7 +30,7 @@ public class LoginDialogController
 		return false;
 	}
 
-	private void Login()
+	private void login()
 	{
 		// TODO Implement Login() method
 	}
