77c5e11a855363696a2cd3fadd7954ef9c7bb8ca
Zoran Jankov <zoran.jankov.87@gmail.com>
2019-12-07 19:08:28 +0100

    Create ButtonName.java
    Stores String values of swing button text.

---
+++ src/com/repair_shop/utility/ButtonName.java
@@ -0,0 +1,24 @@
+package com.repair_shop.utility;
+
+public class ButtonName
+{	
+	public static String LOGIN = "Login";
+	public static String QUIT = "Quit";
+	public static String ADD = "Add";
+	public static String CANCEL = "Cancel";
+	
+	public static String NEW_NOTIFICATION = "New Notification";
+	public static String NOTIFICATION_TYPE = "New Notification Type";
+	public static String SERVICE = "New Service";
+	public static String SERVICE_TYPE = "New Service Type";
+	public static String TICKET = "New Ticket";
+	public static String STATUS = "New Status";
+	public static String NEW_CLIENT = "New Client";
+	public static String MARKETING_TYPE = "New Marketing";
+	public static String LEGAL_ENTITY = "New Legal Entity";
+	public static String DEVICE = "New Device";
+	public static String MODEL = "New Model";
+	public static String DEVICE_TYPE = "New Device Type";
+	public static String BRAND = "New Barnd";
+	public static String USER = "New User";
+}
