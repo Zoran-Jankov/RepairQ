f2860554a6e07619453da3c176eac888fe9099b9
Zoran Jankov <zoran.jankov.87@gmail.com>
2019-12-08 13:43:26 +0100

    Revert "Delete ModelDialogText.java"
    This reverts commit 4c7dc73d2070d46b3d5a3287766e17374629ecb5.

---
+++ src/com/repair_shop/app/utility/ModelDialogText.java
@@ -0,0 +1,15 @@
+package com.repair_shop.app.utility;
+
+public class ModelDialogText
+{
+	public static final String TITLE = "Model Registration Form";
+	public static final String ID_LABEL = "  Model ID";
+	public static final String NAME_LABEL = "* Model name";
+	public static final String DESCRIPTION_LABEL = "  Model specification";
+	public static final String ADD_BUTTON = "Add";
+	public static final String CANCEL_BUTTON = "Cancel";
+	public static final String NAME_ERROR = "<html><font size='2' color='red'>Please enter a uniqe name for the new model </font></html>";
+	
+	public static final String DEVICE_TYPE_ERROR = "<html><font size='2' color='red'>Please select a device type for the new model </font></html>";
+	public static final String BRAND_ERROR = "<html><font size='2' color='red'>Please select a brand for the new model </font></html>";
+}
