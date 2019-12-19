6fbb3ca6082286e4de0ddb8bbccae594b0854a5e
Zoran Jankov <zoran.jankov.87@gmail.com>
2019-12-19 20:24:04 +0100

    Update Brand.java

--- src/main/java/com/yankov/repair_shop/data/entity/Brand.java
+++ src/main/java/com/yankov/repair_shop/data/entity/Brand.java
@@ -2,12 +2,6 @@ package main.java.com.yankov.repair_shop.data.entity;
 
 import main.java.com.yankov.repair_shop.data.EntityType;
 
-/**
- * Class Brand extends abstract class Property and represents
- * a device brand.
- * 
- * @author Zoran Jankov
- */
 public class Brand extends Property
 {
 	@Override
