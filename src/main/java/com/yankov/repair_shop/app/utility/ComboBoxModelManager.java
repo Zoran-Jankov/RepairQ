a88f8ac0366c3adebe4de189e452b65548a08485
Zoran Jankov <zoran.jankov.87@gmail.com>
2019-12-22 19:51:27 +0100

    Update ComboBoxModelManager.java

--- src/main/java/com/yankov/repair_shop/app/utility/ComboBoxModelManager.java
+++ src/main/java/com/yankov/repair_shop/app/utility/ComboBoxModelManager.java
@@ -1,9 +1,12 @@
 package main.java.com.yankov.repair_shop.app.utility;
 
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import javax.swing.ComboBoxModel;
 import javax.swing.DefaultComboBoxModel;
 
 import main.java.com.yankov.repair_shop.data.DataManager;
@@ -13,39 +16,39 @@ import main.java.com.yankov.repair_shop.gui.text.LabelName;
 
 public class ComboBoxModelManager
 {
-	public static DefaultComboBoxModel<String> NOTIFICATION_TYPE = loadModel(EntityType.NOTIFICATION_TYPE);
-	public static DefaultComboBoxModel<String> SERVICE_TYPE = loadModel(EntityType.SERVICE_TYPE);
-	public static DefaultComboBoxModel<String> TICKET = loadModel(EntityType.TICKET);
-	public static DefaultComboBoxModel<String> STATUS = loadModel(EntityType.STATUS);
-	public static DefaultComboBoxModel<String> CLIENT = loadModel(EntityType.CLIENT);
-	public static DefaultComboBoxModel<String> MARKETING = loadModel(EntityType.MARKETING);
-	public static DefaultComboBoxModel<String> DEVICE = loadModel(EntityType.DEVICE);
-	public static DefaultComboBoxModel<String> MODEL = loadModel(EntityType.MODEL);
-	public static DefaultComboBoxModel<String> DEVICE_TYPE = loadModel(EntityType.DEVICE_TYPE);
-	public static DefaultComboBoxModel<String> BRAND = loadModel(EntityType.BRAND);
-	public static DefaultComboBoxModel<String> USER = loadModel(EntityType.USER);
+	public static ComboBoxModel<String> NOTIFICATION_TYPE = loadModel(EntityType.NOTIFICATION_TYPE);
+	public static ComboBoxModel<String> SERVICE_TYPE = loadModel(EntityType.SERVICE_TYPE);
+	public static ComboBoxModel<String> TICKET = loadModel(EntityType.TICKET);
+	public static ComboBoxModel<String> STATUS = loadModel(EntityType.STATUS);
+	public static ComboBoxModel<String> CLIENT = loadModel(EntityType.CLIENT);
+	public static ComboBoxModel<String> MARKETING = loadModel(EntityType.MARKETING);
+	public static ComboBoxModel<String> DEVICE = loadModel(EntityType.DEVICE);
+	public static ComboBoxModel<String> MODEL = loadModel(EntityType.MODEL);
+	public static ComboBoxModel<String> DEVICE_TYPE = loadModel(EntityType.DEVICE_TYPE);
+	public static ComboBoxModel<String> BRAND = loadModel(EntityType.BRAND);
+	public static ComboBoxModel<String> USER = loadModel(EntityType.USER);
 	
-	private static HashMap<EntityType, DefaultComboBoxModel<String>> modelsMap = new HashMap<EntityType, DefaultComboBoxModel<String>>();
+	private static Map<EntityType, ComboBoxModel<String>> MODELS_MAP = new EnumMap<EntityType, ComboBoxModel<String>>(EntityType.class);
 	
 	static
 	{
-		modelsMap.put(EntityType.NOTIFICATION_TYPE, NOTIFICATION_TYPE);
-		modelsMap.put(EntityType.SERVICE_TYPE, SERVICE_TYPE);
-		modelsMap.put(EntityType.TICKET, TICKET);
-		modelsMap.put(EntityType.STATUS, STATUS);
-		modelsMap.put(EntityType.CLIENT, CLIENT);
-		modelsMap.put(EntityType.MARKETING, MARKETING);
-		modelsMap.put(EntityType.DEVICE, DEVICE);
-		modelsMap.put(EntityType.MODEL, MODEL);
-		modelsMap.put(EntityType.DEVICE_TYPE, DEVICE_TYPE);
-		modelsMap.put(EntityType.BRAND, BRAND);
-		modelsMap.put(EntityType.USER, USER);
+		MODELS_MAP.put(EntityType.NOTIFICATION_TYPE, NOTIFICATION_TYPE);
+		MODELS_MAP.put(EntityType.SERVICE_TYPE, SERVICE_TYPE);
+		MODELS_MAP.put(EntityType.TICKET, TICKET);
+		MODELS_MAP.put(EntityType.STATUS, STATUS);
+		MODELS_MAP.put(EntityType.CLIENT, CLIENT);
+		MODELS_MAP.put(EntityType.MARKETING, MARKETING);
+		MODELS_MAP.put(EntityType.DEVICE, DEVICE);
+		MODELS_MAP.put(EntityType.MODEL, MODEL);
+		MODELS_MAP.put(EntityType.DEVICE_TYPE, DEVICE_TYPE);
+		MODELS_MAP.put(EntityType.BRAND, BRAND);
+		MODELS_MAP.put(EntityType.USER, USER);
 	}
 	
 	
-	public static DefaultComboBoxModel<String> loadModel(EntityType entityType)
+	public static ComboBoxModel<String> loadModel(EntityType entityType)
 	{
-		HashSet<String> items = new HashSet<String>();
+		Set<String> items = new HashSet<String>();
 		
 		items.add(LabelName.NULL_ITEM);
 		
@@ -58,7 +61,7 @@ public class ComboBoxModelManager
 			items.addAll(getIdSet(entityType));
 		}
 		
-		DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>
+		ComboBoxModel<String> model = new DefaultComboBoxModel<String>
 												(items.toArray(new String[0]));
 		return model;
 	}
@@ -72,6 +75,7 @@ public class ComboBoxModelManager
 		{
 			dispalyNames.add(id.toString());
 		}
+		
 		return dispalyNames;
 	}
 
@@ -85,7 +89,7 @@ public class ComboBoxModelManager
 		EntityType entityType = newEntity.getEntityType();
 		String item = newEntity.getDisplayName();
 		
-		modelsMap.get(entityType).addElement(item);
-		modelsMap.get(entityType).setSelectedItem(item);
+        ((DefaultComboBoxModel<String>) MODELS_MAP.get(entityType)).addElement(item);
+		MODELS_MAP.get(entityType).setSelectedItem(item);
 	}
 }
