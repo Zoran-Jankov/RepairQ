8cc82a59bd6b4ffc5c58075f2aa94b266df277c2
Zoran Jankov <54670722+EmperorZoran@users.noreply.github.com>
2019-12-30 12:22:49 +0100

    Update UpdateEntityDetails.java

--- src/main/java/com/yankov/repair_shop/app/listener/UpdateEntityDetails.java
+++ src/main/java/com/yankov/repair_shop/app/listener/UpdateEntityDetails.java
@@ -5,11 +5,11 @@ import java.awt.event.ActionListener;
 
 import main.java.com.yankov.repair_shop.app.controller.TicketRegistrationController;
 
-public class UpdateClientDetails implements ActionListener
+public class UpdateEntityDetails implements ActionListener
 {
 	public TicketRegistrationController controller;
 	
-	public UpdateClientDetails(TicketRegistrationController controller)
+	public UpdateEntityDetails(TicketRegistrationController controller)
 	{
 		this.controller = controller;
 	}
