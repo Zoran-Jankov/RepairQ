40e532dc177642f3295965a2fcb1ab5821eef841
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-01-19 15:03:15 +0100

    Update NotificationType.java

--- src/main/java/com/yankov/repair_shop/data/NotificationType.java
+++ src/main/java/com/yankov/repair_shop/data/NotificationType.java
@@ -1,9 +1,21 @@
 package main.java.com.yankov.repair_shop.data;
 
+import main.java.com.yankov.repair_shop.data.entity.Notification;
+
 public enum NotificationType
 {
 	OPEND,
 	TESTED,
 	FINISHED,
 	CLOSED;
+
+	public void addReference(Notification notification)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	public void removeReference(int id)
+	{
+		// TODO Auto-generated method stub
+	}
 }
