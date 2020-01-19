c3c8eb6f5170f9f5bb6c1214174e0210ed60b321
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-01-19 14:05:33 +0100

    Create TableModelManager.java

---
+++ src/main/java/com/yankov/repair_shop/app/utility/TableModelManager.java
@@ -0,0 +1,30 @@
+package main.java.com.yankov.repair_shop.app.utility;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import javax.swing.table.TableModel;
+
+import main.java.com.yankov.repair_shop.data.EntityType;
+
+public class TableModelManager
+{
+	public static TableModel NOTIFICATION_TYPE = loadModel(EntityType.NOTIFICATION_TYPE);
+	public static TableModel SERVICE_TYPE = loadModel(EntityType.SERVICE_TYPE);
+	public static TableModel TICKET = loadModel(EntityType.TICKET);
+	public static TableModel STATUS = loadModel(EntityType.STATUS);
+	public static TableModel CLIENT = loadModel(EntityType.CLIENT);
+	public static TableModel MARKETING = loadModel(EntityType.MARKETING);
+	public static TableModel DEVICE = loadModel(EntityType.DEVICE);
+	public static TableModel MODEL = loadModel(EntityType.MODEL);
+	public static TableModel DEVICE_TYPE = loadModel(EntityType.DEVICE_TYPE);
+	public static TableModel BRAND = loadModel(EntityType.BRAND);
+	public static TableModel USER = loadModel(EntityType.USER);
+	
+	private static Map<EntityType, TableModel> MODELS_MAP = new EnumMap<EntityType, TableModel>(EntityType.class);
+
+	private static TableModel loadModel(EntityType entityType)
+	{
+		return null;
+	}
+}
