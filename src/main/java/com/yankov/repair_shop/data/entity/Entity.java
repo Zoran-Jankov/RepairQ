df7f3feeca513146f42cf44c7a1a4725d1220aa4
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-01-25 15:38:57 +0100

    Update Entity.java

--- src/main/java/com/yankov/repair_shop/data/entity/Entity.java
+++ src/main/java/com/yankov/repair_shop/data/entity/Entity.java
@@ -30,31 +30,6 @@ public interface Entity
 	public void setId(int id);
 	
 	/**
-	 * Getter for entity reference map.
-	 * @return (HashMap<Integer, Entity>) Entity reference map.
-	 */
-	public HashMap<Integer, Entity> getReferenceMap();
-	
-	/**
-	 * Setter for entity reference map.
-	 * @param referenceMap -  Entity reference map.
-	 */
-	public void setReferenceMap(HashMap<Integer, Entity> referenceMap);
-	
-	
-	/**
-	 * Add an entity to this entity reference map.
-	 * @param entity - A entity that is referencing this entity.
-	 */
-	public void addReference(Entity entity);
-	
-	/**
-	 * Removes an entity to this entity reference map.
-	 * @param id - referenced entity id number
-	 */
-	public void removeReference(int id);
-	
-	/**
 	 * Getter for entity display name used in user interface.
 	 * @return (String) Display name for user interface.
 	 */
