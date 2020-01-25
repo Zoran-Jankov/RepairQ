f58e04a5b1c3e4d3f7737cc41f788749c0a17de0
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-01-25 16:14:27 +0100

    Create ContactInfo.java

---
+++ src/main/java/com/yankov/repair_shop/data/component/ContactInfo.java
@@ -0,0 +1,96 @@
+package main.java.com.yankov.repair_shop.data.component;
+
+public class ContactInfo
+{
+	/**
+	 * Person's primary phone number
+	 */
+	private String primePhoneNumber;
+	
+	/**
+	 * Person's alternative phone number
+	 */
+	private String alternativePhoneNumber;
+	
+	/**
+	 * Person's email address
+	 */
+	private String email;
+	
+	/**
+	 * Person's home address
+	 */
+	private String address;
+	
+	/**
+	 * Getter for person's primary phone number.
+	 * @return (String) Person's primary phone number.
+	 */
+	public String getPrimePhoneNumber()
+	{
+		return primePhoneNumber;
+	}
+	
+	/**
+	 * Setter for person's primary phone number.
+	 * @param (String) primePhoneNum - Person's primary phone number.
+	 */
+	public void setPrimePhoneNumber(String primePhoneNumber)
+	{
+		this.primePhoneNumber = primePhoneNumber;
+	}
+
+	/**
+	 * Getter for person's secondary phone number.
+	 * @return (String) Person's secondary phone number.
+	 */
+	public String getAlternativePhoneNumber()
+	{
+		return alternativePhoneNumber;
+	}
+	
+	/**
+	 * Setter for person's alternative phone number.
+	 * @param (String) alternativePhoneNum - Person's alternative phone number.
+	 */
+	public void setAlternativePhoneNumber(String alternativePhoneNumber)
+	{
+		this.alternativePhoneNumber = alternativePhoneNumber;
+	}
+	
+	/**
+	 * Getter for person's email address.
+	 * @return (String) Person's email address.
+	 */
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	/**
+	 * Setter for person's email address.
+	 * @param (String) email - Person's email address.
+	 */
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	
+	/**
+	 * Getter for person's home address.
+	 * @return (String) Person's home address.
+	 */
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	/**
+	 * Setter for person's home address.
+	 * @param (String) address - Person's home address.
+	 */
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+}
