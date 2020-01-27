0a20f098847653d00cfb16d0774c2959f298783e
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-01-27 12:10:02 +0100

    Update DeviceType.java

--- src/main/java/com/yankov/repair_shop/data/entity/DeviceType.java
+++ src/main/java/com/yankov/repair_shop/data/entity/DeviceType.java
@@ -1,5 +1,8 @@
 package main.java.com.yankov.repair_shop.data.entity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import main.java.com.yankov.repair_shop.data.EntityType;
 import main.java.com.yankov.repair_shop.data.component.BasicInfo;
 
@@ -12,20 +15,42 @@ public class DeviceType extends AbstractEntity
 {
 	private BasicInfo deviceType;
 	
+	private Map<Integer, Model> modelsReferencingDeviceType = new HashMap<Integer, Model>();
+	
 	@Override
 	public EntityType getType()
 	{
 		return EntityType.DEVICE_TYPE;
 	}
 	
-	public BasicInfo getBrand()
+	public BasicInfo getDeviceType()
 	{
 		return deviceType;
 	}
 	
-	public void setBrand(BasicInfo deviceType)
+	public void setDeviceType(BasicInfo deviceType)
 	{
 		this.deviceType = deviceType;
+	}
+	
+	public Map<Integer, Model> getModelsReferencingDeviceType()
+	{
+		return modelsReferencingDeviceType;
+	}
+
+	public void setModelsReferencingDeviceType(Map<Integer, Model> modelsReferencingDeviceType)
+	{
+		this.modelsReferencingDeviceType = modelsReferencingDeviceType;
+	}
+	
+	public void addModelReferencingDeviceType(Model model)
+	{
+		modelsReferencingDeviceType.put(model.getId(), model);
+	}
+	
+	public void removeModelReferencingDeviceType(int id)
+	{
+		modelsReferencingDeviceType.remove(id);
 	}
 	
 	@Override
