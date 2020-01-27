7f8be0fcea90a0cfc366c8002d9e4ea95bfa2f23
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-01-27 12:38:48 +0100

    Update User.java

--- src/main/java/com/yankov/repair_shop/data/entity/User.java
+++ src/main/java/com/yankov/repair_shop/data/entity/User.java
@@ -1,9 +1,9 @@
 package main.java.com.yankov.repair_shop.data.entity;
 
 import main.java.com.yankov.repair_shop.data.EntityType;
-import main.java.com.yankov.repair_shop.data.UserType;
 import main.java.com.yankov.repair_shop.data.component.ContactInfo;
 import main.java.com.yankov.repair_shop.data.component.PersonalInfo;
+import main.java.com.yankov.repair_shop.data.component.UserInfo;
 
 /** 
  * Class User represents a employee with associated information about that employee.
@@ -14,7 +14,6 @@ import main.java.com.yankov.repair_shop.data.component.PersonalInfo;
 
 public class User extends AbstractEntity
 {
-	private UserType userType;
 	private PersonalInfo personalInfo;
 	private ContactInfo contactInfo;
 	private UserInfo userInfo;
@@ -24,64 +23,40 @@ public class User extends AbstractEntity
 	{
 		return EntityType.USER;
 	}
-		
-	/**
-	 * Getter for user username.
-	 * @return User username.
-	 */
-	public String getUsername()
+
+	public PersonalInfo getPersonalInfo()
 	{
-		return username;
-	}
-	
-	/**
-	 * Setter for user username.
-	 * @param username - User username.
-	 */
-	public void setUsername(String username)
-	{
-		this.username = username;
-	}
-	
-	/**
-	 * Getter for user user type.
-	 * @return User user type.
-	 */
-	public UserType getUserType()
-	{
-		return userType;
-	}
-	
-	/**
-	 * Setter for user user type.
-	 * @param userType - User user type.
-	 */
-	public void setUserTypes(UserType userTypes)
-	{
-		this.userType = userTypes;
-	}
-	
-	/**
-	 * Getter for user password.
-	 * @return User password.
-	 */
-	public char[] getPassword()
-	{
-		return password;
-	}
-	
-	/**
-	 * Setter for user password.
-	 * @param password - User password.
-	 */
-	public void setPassword(char[] password)
-	{
-		this.password = password;
+		return personalInfo;
 	}
 
+	public void setPersonalInfo(PersonalInfo personalInfo)
+	{
+		this.personalInfo = personalInfo;
+	}
+
+	public ContactInfo getContactInfo()
+	{
+		return contactInfo;
+	}
+
+	public void setContactInfo(ContactInfo contactInfo)
+	{
+		this.contactInfo = contactInfo;
+	}
+
+	public UserInfo getUserInfo()
+	{
+		return userInfo;
+	}
+
+	public void setUserInfo(UserInfo userInfo)
+	{
+		this.userInfo = userInfo;
+	}
+	
 	@Override
 	public String getDisplayName()
 	{
-		return getUsername();
+		return userInfo.getUsername();
 	}
 }
