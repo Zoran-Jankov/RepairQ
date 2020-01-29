318d81d3e407043e3f7834a3483c3a558f3d5261
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-01-29 18:08:16 +0100

    Create CompanyInfoPanel.java

---
+++ src/main/java/com/yankov/repair_shop/gui/panel/CompanyInfoPanel.java
@@ -0,0 +1,33 @@
+package main.java.com.yankov.repair_shop.gui.panel;
+
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import main.java.com.yankov.repair_shop.gui.utility.LabelFactory;
+import java.awt.Font;
+import javax.swing.JTextField;
+
+public class CompanyInfoPanel extends JPanel
+{
+	private static final long serialVersionUID = -8321772559737543683L;
+	private JTextField textField;
+
+	/**
+	 * Create the panel.
+	 */
+	public CompanyInfoPanel()
+	{
+		setLayout(new MigLayout("", "[][grow]", "[25px:n:25px][25px:n:25px]"));
+		
+		JLabel lblTaxIdNumber = LabelFactory.createJLabel("\"Tax ID Number\"", new Font("Tahoma", Font.PLAIN, 13));
+		add(lblTaxIdNumber, "cell 0 0,growy");
+		
+		textField = new JTextField();
+		add(textField, "cell 1 0,grow");
+		textField.setColumns(10);
+		
+		JLabel lblBanckAccount = LabelFactory.createJLabel("Banck Account", new Font("Tahoma", Font.PLAIN, 13));
+		add(lblBanckAccount, "cell 0 1,growy");
+		
+	}
+}
