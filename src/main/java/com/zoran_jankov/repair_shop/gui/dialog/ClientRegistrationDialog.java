02f45f3130ba7290dad90bdea51e3f6168ff7d0b
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-03-02 10:55:29 +0100

    ClientRegistrationDialog update

--- src/main/java/com/zoran_jankov/repair_shop/gui/dialog/ClientRegistrationDialog.java
+++ src/main/java/com/zoran_jankov/repair_shop/gui/dialog/ClientRegistrationDialog.java
@@ -4,9 +4,10 @@ import java.awt.Window;
 
 import javax.swing.JDialog;
 
+import main.java.com.zoran_jankov.repair_shop.gui.panel.CompanyInfoPanel;
+import main.java.com.zoran_jankov.repair_shop.gui.panel.ContactInfoPanel;
 import main.java.com.zoran_jankov.repair_shop.gui.panel.IdPanel;
 import main.java.com.zoran_jankov.repair_shop.gui.panel.InputButtonPanel;
-import main.java.com.zoran_jankov.repair_shop.gui.panel.MarketingPanel;
 import main.java.com.zoran_jankov.repair_shop.gui.panel.PersonalInfoPanel;
 import main.java.com.zoran_jankov.repair_shop.gui.text.WindowTitle;
 import net.miginfocom.swing.MigLayout;
@@ -16,7 +17,8 @@ public class ClientRegistrationDialog extends JDialog implements InputDialog
 	private static final long serialVersionUID = -394107433140693140L;
 	private IdPanel idPanel = new IdPanel();
 	private PersonalInfoPanel personalInfoPanel = new PersonalInfoPanel();
-	private MarketingPanel marketingPanel = new MarketingPanel();
+	private ContactInfoPanel contactInfoPanel = new ContactInfoPanel();
+	private CompanyInfoPanel companyInfoPanel = new CompanyInfoPanel();
 	private InputButtonPanel buttonPanel = new InputButtonPanel();
 	
 	public ClientRegistrationDialog(Window owner)
@@ -27,15 +29,17 @@ public class ClientRegistrationDialog extends JDialog implements InputDialog
 		setTitle(WindowTitle.NEW_CLIENT);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
-		getContentPane().setLayout(new MigLayout("", "[434px]", "[25px:n][][][]"));
+		getContentPane().setLayout(new MigLayout("", "[434px]", "[25px:n][][][][][]"));
 		
 		getContentPane().add(idPanel, "cell 0 0,grow");
 		
 		getContentPane().add(personalInfoPanel, "cell 0 1,grow");
 		
-		getContentPane().add(marketingPanel, "cell 0 2,grow");
+		getContentPane().add(contactInfoPanel, "cell 0 2,grow");
 		
-		getContentPane().add(buttonPanel, "cell 0 3,grow");
+		getContentPane().add(companyInfoPanel, "cell 0 3,grow");
+		
+		getContentPane().add(buttonPanel, "cell 0 5,grow");
 		
 		pack();
 	}
@@ -49,11 +53,6 @@ public class ClientRegistrationDialog extends JDialog implements InputDialog
 	public PersonalInfoPanel getPersonalInfoPanel()
 	{
 		return personalInfoPanel;
-	}
-	
-	public MarketingPanel getMarketingPanel()
-	{
-		return marketingPanel;
 	}
 
 	@Override
