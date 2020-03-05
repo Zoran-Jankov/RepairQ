be0d98d1ee94d0ba5ec260d4b9f4e4fca0429fa0
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-03-05 09:11:41 +0100

    Update UserInputData.java

--- src/main/java/com/zoran_jankov/repair_shop/data/embeddable/UserInputData.java
+++ src/main/java/com/zoran_jankov/repair_shop/data/embeddable/UserInputData.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import main.java.com.zoran_jankov.repair_shop.data.entity.User;
 
@@ -11,7 +13,30 @@ import main.java.com.zoran_jankov.repair_shop.data.entity.User;
 public class UserInputData
 {
 	@Column(name = "user")
+	private
 	User user;
 	
+	@Column(name = "creation_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private
 	LocalDateTime timestmp;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user)
+	{
+		this.user = user;
+	}
+
+	public LocalDateTime getTimestmp()
+	{
+		return timestmp;
+	}
+
+	public void setTimestmp(LocalDateTime timestmp)
+	{
+		this.timestmp = timestmp;
+	}
 }
