af40d602d9061abd2e48fa66791e53c267a196dd
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-02-03 22:00:22 +0100

    Update UpdateInfo.java

--- src/main/java/com/zoran_jankov/repairq/data/embeddable/UpdateInfo.java
+++ src/main/java/com/zoran_jankov/repairq/data/embeddable/UpdateInfo.java
@@ -10,24 +10,26 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import com.zoran_jankov.repairq.data.DataManager;
 import com.zoran_jankov.repairq.data.entity.User;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
 @Embeddable
 public class UpdateInfo {
+    @Column(name = "version",
+	    nullable = false,
+	    updatable = true)
+    @Setter(AccessLevel.PRIVATE)
+    private short version;
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "last_updated_by_user_id",
     		nullable = false,
     		updatable = true)
     private User user;
-    
-    @Column(name = "version",
-	    nullable = false,
-	    updatable = true)
-    private short version;
 
     @Column(name = "last_update_date",
 	    nullable = false,
@@ -36,14 +38,14 @@ public class UpdateInfo {
     private LocalDateTime lastUpdateDate;
     
     public UpdateInfo(User user, LocalDateTime lastUpdateDate) {
-	this.user = user;
-	this.version = 1;
-	this.lastUpdateDate = lastUpdateDate;
+	setVersion((short) 1);
+	setUser(user);
+	setLastUpdateDate(lastUpdateDate);
     }
     
     public void update(User user, LocalDateTime lastUpdateDate) {
-	this.user = user;
-	this.version ++;
-	this.lastUpdateDate = lastUpdateDate;
+	setVersion(version ++);
+	setUser(user);
+	setLastUpdateDate(lastUpdateDate);
     }
 }
