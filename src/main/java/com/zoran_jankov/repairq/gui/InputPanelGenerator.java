ccbda64986f14dc108632a717ca02eb9470fb33b
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-07-05 23:44:08 +0200

    Update InputPanelGenerator.java

--- src/main/java/com/zoran_jankov/repairq/gui/InputPanelGenerator.java
+++ src/main/java/com/zoran_jankov/repairq/gui/InputPanelGenerator.java
@@ -1,8 +1,20 @@
 package com.zoran_jankov.repairq.gui;
 
+import java.lang.reflect.Field;
+
+import org.hibernate.mapping.Column;
+
+import com.zoran_jankov.repairq.app.FieldProperties;
 import com.zoran_jankov.repairq.data.entity.BaseEntity;
 
-public class InputPanelGenerator {
+public class InputPanelGenerator {  
     public InputPanelGenerator(Class<? extends BaseEntity> type) {
+	 Field[] fields = type.getClass().getDeclaredFields();
+	 for (Field field : fields) {
+	     var column = field.getAnnotation(Column.class);
+	     FieldProperties fieldProperties = new FieldProperties();
+	}
     }
+    
+    
 }
